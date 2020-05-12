5918958d442d5ba7779df69de37844b2a0a2cd43
Eas <me@eas.web.id>
2020-05-12 13:07:14 +0700

    update git repo

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Auto-deploy from Travis CI build $TRAVIS_BUILD_NUMBER"
-git remote add deploy https://$GH_TOKEN@github.com/easterneas/gbi-grogol > /dev/null 2>&1
+git remote add deploy https://$GH_TOKEN@github.com/easterneas/easterneas.github.io > /dev/null 2>&1
 git push --force deploy master > /dev/null 2>&1
